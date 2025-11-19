8c74cfd0b70c02df7ae853da310726b668df6616
awsragook2 <awsragook2@gmail.com>
2025-11-19 16:54:28 -0500

    Add CodeBuild customizations IAM role data source

--- sources/aft-customizations-common/templates/customizations_pipeline/data.tf
+++ sources/aft-customizations-common/templates/customizations_pipeline/data.tf
@@ -45,3 +45,7 @@ data "aws_s3_bucket" "aft_codepipeline_customizations_bucket" {
 data "aws_ssm_parameter" "vcs_provider" {
   name = "/aft/config/vcs/provider"
 }
+---Added By Ragu
+data "aws_iam_role" "aft_codebuild_customizations_role" {
+  name = "aft-codebuild-customizations-role"
+}
